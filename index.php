8cf70bd908407da1dac7b5080689e83f369a0cb5
kimsb <kim.stephen.bovim@bekk.no>
2016-08-23 22:42:50 +0200

    bytter om på fnuttene i queryen

--- index.php
+++ index.php
@@ -325,7 +325,7 @@
 								}
 								
 								
-								$sql = 'SELECT * FROM dictionary WHERE alpha IN ("' . implode('","', $list) . '" COLLATE utf8_swedish_ci)';
+								$sql = "SELECT * FROM dictionary WHERE alpha IN ('" . implode("','", $list) . "' COLLATE utf8_swedish_ci)";
 
                                 $result = pg_exec($db_conn, $sql) or die('Query failed: ' . pg_last_error());
 								if (pg_numrows($result) !== 0) {
