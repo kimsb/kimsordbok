234d487827e6c0acb098b2bb9c22eb016238cb1b
kimsb <kim.stephen.bovim@bekk.no>
2016-08-26 20:31:09 +0200

    legger til script for å sjekke rating i scrabbeller

---
+++ bin/scrabbeller.php
@@ -0,0 +1,66 @@
+<?php
+require 'vendor/autoload.php';
+
+function send_notification_email($receiver, $message)
+{
+    $sendgrid = new SendGrid(getenv("SENDGRID_USERNAME"), getenv("SENDGRID_PASSWORD"));
+    $email = new SendGrid\Email();
+    $email->addTo($receiver)
+        ->setFrom('kimbovim@gmail.com')
+        ->setSubject('Scrabbeller er oppdatert!')
+        ->setText($message);
+
+    $sendgrid->send($email);
+}
+
+function get_string_between($string, $start, $end)
+{
+    $string = " " . $string;
+    $ini = strpos($string, $start);
+    if ($ini == 0) return "";
+    $ini += strlen($start);
+    $len = strpos($string, $end, $ini) - $ini;
+    return substr($string, $ini, $len);
+}
+
+function perform_diff()
+{
+    $url = parse_url(getenv("HEROKU_POSTGRESQL_AQUA_URL"));
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+
+    $db_conn = pg_connect("user=$username password=$password host=$host sslmode=require dbname=$database") or die('Could not connect: ' . pg_last_error());
+    pg_query("SET NAMES 'utf8'");
+
+    $sql = "SELECT * FROM scrabbeller";
+    $result = pg_exec($db_conn, $sql) or die('Query failed: ' . pg_last_error());
+    if (pg_numrows($result) !== 0) {
+        while ($row = pg_fetch_array($result)) {
+            $contents = file_get_contents($row[url]);
+            if ($contents !== FALSE) {
+                $newscore = str_replace(',', '.', get_string_between($contents, "Rating:</span> ", "<br />"));
+                if (strcmp($row[score], $newscore) != 0) {
+                    $update = "UPDATE scrabbeller SET score='$newscore'";
+                    pg_exec($db_conn, $update) or die('Query failed: ' . pg_last_error());
+
+                    $message = "Hei, det har akkurat skjedd en endring i ratingen din på Scrabbeller!<br><br>";
+                    $scorediff = $newscore - $row[score];
+                    if ($scorediff > 0) {
+                        $message .= "Godt jobba! Du har gått opp $scorediff poeng!<br><br>";
+                    } else {
+                        $scorediff = abs($scorediff);
+                        $message .= "Auda, du har gått ned $scorediff poeng...<br><br>";
+                    }
+                    $message .= "Gå til <a href='" . $row[url] . "'>Scrabbeller</a> for å se alle oppdateringer.";
+                    send_notification_email($row[email], $message);
+                }
+            }
+        }
+    }
+}
+
+perform_diff();
+
+?>
