07d85a3eec98476efc8b4a9a429759482a0a3e8f
kimsb <kim.stephen.bovim@bekk.no>
2016-10-04 18:16:24 +0200

    legger til script for å sende dagens ord fra NSF sine sider

---
+++ bin/dagensord.php
@@ -0,0 +1,32 @@
+<?php
+require 'vendor/autoload.php';
+
+function send_notification_email($receiver, $message)
+{
+    $sendgrid = new SendGrid(getenv("SENDGRID_USERNAME"), getenv("SENDGRID_PASSWORD"));
+    $email = new SendGrid\Email();
+    $email->addTo($receiver)
+        ->setFrom(getenv("MAIL_SENDER_ADDRESS"))
+        ->setSubject('Dagens ord')
+        ->setHtml($message);
+
+    $sendgrid->send($email);
+}
+
+function get_string_between($string, $start, $end)
+{
+    $string = " " . $string;
+    $ini = strpos($string, $start);
+    if ($ini == 0) return "";
+    $ini += strlen($start);
+    $len = strpos($string, $end, $ini) - $ini;
+    return substr($string, $ini, $len);
+}
+
+$htmlpage = file_get_contents(getenv("SCRABBLEFORBUNDET_URL"));
+$dagensord = get_string_between($htmlpage, "<b>Dagens ord<b></h3>", "</li>");
+$message = "<h2>Dagens ord fra NSF:</h2><br><br>>";
+$message .= $dagensord . ".<br><br>Kim";
+send_notification_email(getenv("MAIL_SENDER_ADDRESS"), $message);
+
+?>
