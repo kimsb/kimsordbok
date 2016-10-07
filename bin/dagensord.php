23e677317a0e450c804095c42a23054152259202
kimsb <kim.stephen.bovim@bekk.no>
2016-10-07 07:41:28 +0200

    det var et tilfelle der dagens ord hadde space på slutten

--- bin/dagensord.php
+++ bin/dagensord.php
@@ -26,7 +26,7 @@ function get_string_between($string, $start, $end)
 $htmlpage = file_get_contents(getenv("SCRABBLEFORBUNDET_URL"));
 $dagensord = get_string_between($htmlpage, "<b>Dagens ord<b></h3>", "</li>");
 $message = "<p>Dagens ord fra NSF:</p><br>";
-$message .= $dagensord . ".<br><br>Kim";
+$message .= trim($dagensord) . ".<br><br>Kim";
 send_notification_email(getenv("MAIL_SENDER_ADDRESS"), $message);
 
 ?>
